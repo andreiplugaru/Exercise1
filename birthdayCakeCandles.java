d23a9e09897f3d4818c2f5ec49e2940494174ce2
Andrei Plugaru <andrei.p2001@outlook.com>
2022-05-08 14:31:26 +0300

    birthdayCakeCandles

---
+++ birthdayCakeCandles.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class birthdayCakeCandles {
+    public static void main(String[] args) {
+        int n;
+        int max = -1, maxCandles = 0, current;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        for(int i = 0; i < n; i++){
+            current = scanner.nextInt();
+            if(current > max)
+            {
+
+                
+                max = current;
+                maxCandles = 1;
+            }
+            else if(current == max)
+                maxCandles++;
+        }
+        System.out.println(maxCandles);
+
+    }
+}
